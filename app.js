6820a2c2a20db4c812a0968248c058cea4774421
fajrCode <fajri42vandi@gmail.com>
2024-03-17 23:03:08 +0700

    update: define error handling 404

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
 const app = express();
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs')
+const apiDocs = YAML.load('./api-docs.yaml')
 
 //Middleware
-const { isAuthenticated } = require("./middleware/isAuthenticated");
+const notFoundHandler = require("./middleware/errors");
 
 //Router
 const indexRouter = require("./routes/index/index.router");
@@ -13,7 +16,13 @@ app.use(express.json());
 app.use(morgan("dev"));
 app.use(cors());
 
+//API DOCS with Swagger
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(apiDocs))
+
 //Routes
-app.use("/", isAuthenticated, indexRouter);
+app.use("/", indexRouter);
+
+// Routes Not found
+app.use(notFoundHandler);
 
 module.exports = app;
